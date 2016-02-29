db3acd50ab5cf35ec9554208cbd8a9261b27d0e0
Lalit Tanwar <lalit.tanwar07@gmail.com>
2016-02-29 13:48:04 +0530

    local throttle added

--- lazy.js
+++ lazy.js
@@ -51,9 +51,41 @@
     };
   };
 
+  var throttle = function(func, wait, options) {
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    if (!options) options = {};
+    var later = function() {
+      previous = options.leading === false ? 0 : _.now();
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    };
+    return function() {
+      var now = _.now();
+      if (!previous && options.leading === false) previous = now;
+      var remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0 || remaining > wait) {
+        if (timeout) {
+          clearTimeout(timeout);
+          timeout = null;
+        }
+        previous = now;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
+    };
+  };
+
   foo.lazy.throttle = function(func, wait, options){
     var context,myDeffered = $.Deferred();
-    var defResp = _.throttle(function(){
+    var defResp = throttle(function(){
       var resp = func.apply(context, arguments);
       if(isPromise(resp)){
         resp.done(function(resp2){
